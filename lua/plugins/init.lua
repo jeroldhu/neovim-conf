f596486324a739f8647cb69a45ec74f6cc52d325
Jerold <jeroldhu@hotmail.com>
2024-05-17 17:00:45 +0800

    增加chatgpt配置

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -254,7 +254,29 @@ local plugins = {
       require("plugins.others").bufferline()
     end
   },
-
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup({
+        openai_params = {
+          model = "gpt-4",
+          frequency_penalty = 0,
+          presence_penalty = 0,
+          max_tokens = 1024,
+          temperature = 0,
+          top_p = 1,
+          n = 1,
+        },
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "folke/trouble.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  }
   -- {
   --   'natecraddock/workspaces.nvim',
   --   config = function()
